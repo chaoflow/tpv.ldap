e5f8a549dde85b57d7b067fbcf990998d9a51d94
Florian Friesdorf <flo@chaoflow.net>
2013-05-16 15:24:39 +0200

    by default '*' and 'memberOf'

--- src/tpv/ldap/_directory.py
+++ src/tpv/ldap/_directory.py
@@ -90,8 +90,7 @@ class Directory(object):
             filter = filter & filterstr
             filterstr = unicode(filter)
         if attrlist is None:
-            attrlist = ['']  # meaning: no attributes, ['*'] all, ['+'] internal
-            attrlist = ['*']  # XXX: only temporarily
+            attrlist = ['*', 'memberOf']
         return (
             self.Child(dn=dn, attributes=attributes, directory=self)
             for dn, attributes in self.ldap.search_s(base=base, scope=scope,
